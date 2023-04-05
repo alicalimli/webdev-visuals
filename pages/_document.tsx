30a3acb4a6be89cc301514c0f19c81c575afa9b6
alicalimli <alicalimli76@gmail.com>
2023-04-05 20:56:48 +0300

    [FEAT] Footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components";
+import Footer from "@/components/footer/Footer";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -10,7 +11,10 @@ export default function Document() {
       </Head>
       <body className="bg-bg-main text-white ">
         <Navbar />
-        <Main />
+        <div className="min-h-screen">
+          <Main />
+        </div>
+        <Footer />
         <NextScript />
       </body>
     </Html>
