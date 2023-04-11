ba85e7edea6eafefe0798b229785f72ddfcd46c7
alicalimli <alicalimli76@gmail.com>
2023-04-11 08:01:42 +0300

    [FEAT] Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "@/components";
 import Footer from "@/components/footer/Footer";
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
     <Html lang="en">
