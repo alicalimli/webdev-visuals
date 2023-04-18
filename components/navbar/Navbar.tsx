cdcff70bb51c5a0e376a330b471c3d5145f50071
alicalimli <alicalimli76@gmail.com>
2023-04-18 18:50:08 +0300

    navbar adjustments

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 const links = [
   {
+    label: "Collections",
+    href: "/collections",
+  },
+  {
     label: "Visuals",
     href: "/visuals",
   },
@@ -23,15 +27,14 @@ const Navbar = ({}: NavbarProps) => {
 
   return (
     <nav className="flex w-full items-center gap-2 p-8 px-vw-32 ">
-      <Link href="/" className="hidden font-bold fluid-2xl sm:block">
-        WebDevVisuals
+      <Link href="/" className="hidden fluid-2xl sm:block text-slate-300">
+        webdev
+        <span className="font-semibold text-white">visuals</span>
       </Link>
-      <Link href="/" className="font-bold fluid-2xl sm:hidden">
+      <Link href="/" className="font-bold fluid-xl sm:hidden">
         WDV
       </Link>
-      {/* <ul className='ml-auto flex gap-4 text-xl'>
-        {renderLinks}
-      </ul> */}
+      <ul className="ml-auto flex gap-4 fluid-lg">{renderLinks}</ul>
     </nav>
   );
 };
